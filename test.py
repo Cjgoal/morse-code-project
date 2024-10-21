7b97363185a5c367f7da835be943c0d9903a17ec
Cjgoal <87348645+Cjgoal@users.noreply.github.com>
2024-10-21 12:06:40 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,5 @@
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(21,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-GPIO.setup(26, GPIO.OUT)
-
-try:
-	while True:
-		if GPIO.input(21) == GPIO.LOW:
-			GPIO.output(26, GPIO.LOW)
-
-		else:
-			GPIO.output(26, GPIO.HIGH)
-except KeyboardInterrupt:
-	pass
-finally:
-	GPIO.cleanup()
-
 MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
                     'C':'-.-.', 'D':'-..', 'E':'.',
                     'F':'..-.', 'G':'--.', 'H':'....',
@@ -33,6 +15,25 @@ MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
                     '0':'-----', ', ':'--..--', '.':'.-.-.-',
                     '?':'..--..', '/':'-..-.', '-':'-....-',
                     '(':'-.--.', ')':'-.--.-'}
+message = "'
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(26, GPIO.OUT)
+
+def main():
+	try:
+		while True:
+			if GPIO.input(21) == GPIO.LOW:
+				GPIO.output(26, GPIO.LOW)
+	
+			else:
+				GPIO.output(26, GPIO.HIGH)
+	except KeyboardInterrupt:
+		pass
+	finally:
+		GPIO.cleanup()
+
+
  
 
 def decrypt(message):
